bd72120ef3530c42ce1fa49596c6a033e61744ce
RAM <ram@billionlives.in>
2022-05-26 11:23:43 +0530

    Some  Changes

--- src/Components/StateVariables.js
+++ src/Components/StateVariables.js
@@ -6,66 +6,40 @@ class StateVariables extends React.Component {
     color2: "",
   };
 
-  handleChange = (e) => {
-    this.setState({
-      color1: e.target.value,
-    });
-  };
-
   render() {
     let inpColor = "";
 
     return (
       <>
-        <div
-          style={{
-            width: 500,
-            border: 1,
-            borderColor: "black",
-            borderRadius: 0.5,
-          }}
-        >
-          <div>
-            <h3>Textbox Onchange Event</h3>
-            <div style={{ backgroundColor: this.state.color1, width: 400 }}>
-              <input
-                type="text"
-                name="username"
-                placeholder="Enter the Color"
-                onChange={this.handleChange}
-              />
-            </div>
-            <div style={{ backgroundColor: this.state.color1 }}></div>
+        <div>
+          <h3>Textbox Onchange Event</h3>
+          <div style={{ backgroundColor: this.state.color1, width: 400 }}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter the Color"
+              onChange={(e) => this.setState({ color1: e.target.value })}
+            />
           </div>
         </div>
         <br />
         <hr />
-        <div
-          style={{
-            width: 500,
-            border: 1,
-            borderColor: "grey",
-            borderRadius: 0.5,
-          }}
-        >
-          <div>
-            <h3>Enter color and Submit </h3>
-            <div style={{ backgroundColor: this.state.color2, width: 400 }}>
-              <input
-                type="text"
-                name="user"
-                placeholder="Enter Color and submit it"
-                onChange={(e) => {
-                  inpColor = e.target.value;
-                }}
-              />
-              <button
-                name="submit"
-                onClick={(e) => this.setState({ color2: inpColor })}
-              >
-                Click Here
-              </button>
-            </div>
+        <div>
+          <h3>Enter color and Click the button </h3>
+
+          <div style={{ backgroundColor: this.state.color2, width: 400 }}>
+            <input
+              type="text"
+              name="user"
+              placeholder="Enter Color and submit it"
+              onChange={(e) => (inpColor = e.target.value)}
+            />
+            <button
+              name="submit"
+              onClick={(e) => this.setState({ color2: inpColor })}
+            >
+              Click Here
+            </button>
           </div>
         </div>
       </>
